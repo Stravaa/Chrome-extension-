9e219c55ee1ad9dc4d40146d045ecd978e410bfd
Zineb Elhoummany <zineb.elhoummany13@gmail.com>
2023-12-29 14:05:56 +0100

    styled buttons when hover & active. Added functionality to capture the url of the active tab and store it in myLeads and local storage

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const inputEl = document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 function render(leads) {
   let listItems = "";
